25b54e9da32f0948f065659285b22839fb8718ab
unknown <deepakumar50136@gmail.com>
2021-07-11 21:46:26 +0530

    jwt

--- APi/urls.py
+++ APi/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from rest_framework.authtoken import views
+
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('my_app.urls'))
+
+    path('', include('my_app.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -4,17 +4,36 @@ from rest_framework import serializers
 
 from rest_framework.response import Response
 
-from .serializers import StudentSerializer
-from rest_framework.decorators import api_view
-from .models import Student
+from .serializers import *
+from rest_framework.decorators import api_view, permission_classes
+from .models import *
+
+from rest_framework.authentication import TokenAuthentication
+
+
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 @api_view(["GET"])
+@permission_classes([IsAuthenticated])
 def home(request):
+    permission_classes = [TokenAuthentication]
     student = Student.objects.all()
     json = StudentSerializer(student, many=True)
     return Response({'status': 200, "message": "successful", "data": json.data})
 # Create your views here.
+
+
+@api_view(["GET"])
+def GetBook(request):
+    book = Book.objects.all()
+    serializer = BookSerializer(book, many=True)
+    # if serializer.is_valid():
+
+    return Response({"status": 200, "message": "successful", "data": serializer.data})
+
+    # return Response({"status": 403, "message": "not found"})
 
 
 @api_view(["POST"])
@@ -30,4 +49,49 @@ def post_student(request):
         return Response({'status': 200, "message": "successful", "data": serializer.data})
     else:
         print(serializer.data)
-        return Response({'status': 403, "message": "something went wrong"})
+        return Response({'status': 403, "message": "something went wrong",  "error": serializer.errors})
+
+
+@api_view(["PUT"])
+def Update(request, id):
+
+    try:
+
+        student_obj = Student.objects.get(id=id)
+        print(student_obj, "this is object")
+        print(request.data)
+        # set partial=True to update a data partially
+        serializer = StudentSerializer(
+            student_obj, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": 200, "message": "successful", "data": serializer.data})
+
+        return Response({"status": 403, "errors": serializer.errors})
+
+    except Exception as e:
+        return Response({"status": 403, "message": "invalid_id"})
+
+
+# IN put method every field is need to be passed for updation of data
+# while in  Patch  method only one field is passed for updation. we will pass an argument in response "partial = True"
+
+
+@api_view(["DELETE"])
+def Del_student(request, id):
+
+    try:
+
+        student_obj = Student.objects.get(id=id)
+        print(request.data)
+        # set partial=True to update a data partially
+        student_obj.delete()
+
+        return Response({"status": 200, "message": "deleted"})
+
+    except Exception as e:
+        return Response({"status": 403, "message": "invalid_id"})
+
+
+# create
